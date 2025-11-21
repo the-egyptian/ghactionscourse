0fa98d9e2a95d9b6d01186675765c02791596267
the-egyptian <theegyptian.work1@gmail.com>
2025-11-21 18:03:39 +0200

    Update NodeJs app

--- src/test.sh
+++ src/test.sh
@@ -3,7 +3,7 @@
 #src/test.sh
 EXPECTED="Hello, Test!"
 
-OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
+OUTPUT=$(node -e "console.log(require('./src/app')('Test!'))")
 
 if [ "$OUTPUT" == "$EXPECTED" ]; then
     echo "Test passed!"
